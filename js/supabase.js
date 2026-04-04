1f07ecde25f896c4c475a235571d8ead1001edab
acesse-site <daquireclame@gmail.com>
2026-04-04 17:22:14 -0400

    Delete js directory

--- js/supabase.js
+++
@@ -1,9 +0,0 @@
-// Supabase client
-const { createClient } = supabase;
-const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-// Helper: get current user
-async function getCurrentUser() {
-  const { data: { user } } = await db.auth.getUser();
-  return user;
-}
